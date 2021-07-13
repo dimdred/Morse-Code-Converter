6f590b241bfc2ee30b5783f530945406771b7d75
Dzmitry_Dashkevich <dimdred@tut.by>
2021-07-13 08:24:58 -0400

    Create String to Morse Code converter

---
+++ main.py
@@ -0,0 +1,49 @@
+MORSE_CODE = {
+    'a': '.-',
+    'b': '-...',
+    'c': '-.-.',
+    'd': '-..',
+    'e': '.',
+    'f': '..-.',
+    'g': '--.',
+    'h': '....',
+    'i': '..',
+    'j': '.---',
+    'k': '-.-',
+    'l': '.-..',
+    'm': '--',
+    'n': '-.',
+    'o': '---',
+    'p': '.--.',
+    'q': '--.-',
+    'r': '.-.',
+    's': '...',
+    't': '-',
+    'u': '..-',
+    'v': '...-',
+    'w': '.--',
+    'x': '-..-',
+    'y': '-.--',
+    'z': '--..',
+    '1': '.----',
+    '2': '..---',
+    '3': '...--',
+    '4': '....-',
+    '5': '.....',
+    '6': '-....',
+    '7': '--...',
+    '8': '---..',
+    '9': '----.',
+    '0': '-----'
+}
+
+
+def text_to_morse(st: str):
+    result = ''
+    # split string to words
+    words = st.split(' ')
+    for word in words:
+        symbols_list = list(word.lower())
+        morse_list = [MORSE_CODE[symbol] for symbol in symbols_list]
+        result += ' '.join(morse_list) + ' / '  # / is word separator
+    return result[:-3]
